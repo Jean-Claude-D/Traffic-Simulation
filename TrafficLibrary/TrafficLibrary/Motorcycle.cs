386a44e75f13f83894197408e6f7bdc597fb0a5b
Benjamin Kearney <benjaminjameskearney@gmail.com>
2018-03-26 19:28:26 +0000

    Vehicles

---
+++ TrafficLibrary/TrafficLibrary/Motorcycle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrafficLibrary
+{
+    public class Motorcycle:Vehicle
+    {
+        public Motorcycle(Grid grid): base(2, 1, 1, grid)
+        {
+        }
+        public Motorcycle(Grid grid, int x, int y) : base(2, 1, 1, grid, x, y)
+        {
+        }
+    }
+}
