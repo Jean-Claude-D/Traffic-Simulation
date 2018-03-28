78e7836bc13b9b4781ab3f57bea1cefae89a0526
Alexander Kofman <alexander.kofman@dawsoncollege.qc.ca>
2018-03-27 20:43:43 -0400

    Initial vehicle test

---
+++ TrafficLibrary/TrafficTest/CarTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TrafficLibrary;
+
+namespace TrafficTest
+{
+    [TestClass]
+    public class CarTest
+    {
+        [TestMethod]
+        public void TestConstructor()
+        {
+            Car someCar;
+            Assert.ThrowsException<ArgumentException>(() => someCar = new Car(null));
+
+            Tile t = new Grass();
+            Tile t2 = new Grass();
+            Tile t3 = new Grass();
+            Tile t4 = new Grass();
+
+            Tile[,] tGrid = new Tile[2, 2];
+
+            tGrid[0, 0] = t;
+            tGrid[0, 1] = t2;
+            tGrid[1, 0] = t3;
+            tGrid[1, 1] = t4;
+
+            Grid someGrid = new Grid(tGrid);
+            Car okCar = new Car(5,2,3,someGrid);
+            Assert.AreEqual();
+
+        }
+    }
+}
---
+++ TrafficLibrary/TrafficTest/VehicleTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TrafficTest
+{
+    [TestClass]
+    public class VehicleTest
+    {
+        [TestMethod]
+        public void TestConstructor()
+        {
+            try
+            {
+                VehicleTest v = new V(12.34, 12.34, 5, null);
+                
+            }
+            catch (ArgumentException e) { }
+        }
+    }
+}
