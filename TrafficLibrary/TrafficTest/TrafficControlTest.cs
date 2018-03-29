f2acb2edbc064373a16fe0ed8259fee13ac659c6
Quoc Huan Nguyen <quoc.nguyen@dawsoncollege.qc.ca>
2018-03-29 18:31:42 -0400

    Add some tests for TrafficControlTest.cs

--- TrafficLibrary/TrafficTest/TrafficControlTest.cs
+++ TrafficLibrary/TrafficTest/TrafficControlTest.cs
@@ -11,11 +11,39 @@ namespace TrafficTest
         [TestMethod]
         public void TestParse()
         {
-            string content = File.ReadAllText(@"H:\C#\Traffic\TrafficLibrary\TrafficTest\trafficfile.txt");
+            string file1 = File.ReadAllText(@"..\..\..\trafficfile.txt");
+            string file2 = File.ReadAllText(@"..\..\..\TrafficTest\Properties\grid.txt");
 
             TrafficControl tc = new TrafficControl();
 
-            tc.Parse(content);
+            tc.Parse(file1);
+            
+            Assert.AreEqual(tc.Grid[0, 0].GetType(), typeof(Grass));
+            Assert.AreEqual(tc.Grid[1, 1].GetType(), typeof(Grass));
+            Assert.AreEqual(tc.Grid[2, 2].GetType(), typeof(Light));
+            Assert.AreEqual(tc.Grid[2, 5].GetType(), typeof(Light));
+            Assert.AreEqual(tc.Grid[3, 3].GetType(), typeof(IntersectionTile));
+            Assert.AreEqual(tc.Grid[3, 4].GetType(), typeof(IntersectionTile));
+            Assert.AreEqual(tc.Grid[3, 5].GetType(), typeof(Road));
+            Assert.AreEqual(tc.Grid[3, 5].Direction, Direction.Down);
+            Assert.AreEqual(tc.Grid[5, 3].Direction, Direction.Left);
+            Assert.AreEqual(tc.Grid[0, 5].Direction, Direction.Right);
+            Assert.AreEqual(tc.Grid[4, 0].Direction, Direction.Up);
+            Assert.IsTrue(tc.Grid.InBounds(7, 7));
+            Assert.IsFalse(tc.Grid.InBounds(8, 8));
+            
+
+            TrafficControl tc2 = new TrafficControl();
+
+            tc2.Parse(file2);
+            //
+            Assert.AreEqual(tc2.Grid[0, 0].GetType(), typeof(Grass));
+        }
+
+        [TestMethod]
+        public void TestUpdate()
+        {
+
         }
     }
 }
