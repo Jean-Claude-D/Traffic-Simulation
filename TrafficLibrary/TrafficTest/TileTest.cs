f64711e8305484c35955ed20f09da824cf3e8bc9
DesJC <jcd9930@hotmail.ca>
2018-03-29 18:58:39 -0400

    Add TileTest and TileMock in TrafficTest

---
+++ TrafficLibrary/TrafficTest/TileTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrafficTest
+{
+    public class TileTest
+    {
+    }
+
+    public class TileMock
+    {
+
+    }
+}
