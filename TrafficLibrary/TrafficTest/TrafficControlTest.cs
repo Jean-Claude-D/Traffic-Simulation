3f0e61a14ee26ebec57dc0a79d1b2eaf114c6ef1
Quoc Huan Nguyen <quoc.nguyen@dawsoncollege.qc.ca>
2018-03-29 15:55:26 -0400

    Setup TrafficControl testing

---
+++ TrafficLibrary/TrafficTest/TrafficControlTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TrafficLibrary;
+
+namespace TrafficTest
+{
+    [TestClass]
+    public class TrafficControlTest
+    {
+        [TestMethod]
+        public void TestParse()
+        {
+            string content = File.ReadAllText(@"H:\C#\Traffic\TrafficLibrary\TrafficTest\trafficfile.txt");
+
+            TrafficControl tc = new TrafficControl();
+
+            tc.Parse(content);
+        }
+    }
+}
