50bddd7c13731da50ae6422cecc0a63d9a204671
DesJC <jcd9930@hotmail.ca>
2018-03-29 19:51:07 -0400

    Add TileTest Class (now a Test class)

---
+++ TrafficLibrary/TrafficTest/TileTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TrafficTest
+{
+    [TestClass]
+    public class TileTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
