49fcd54d74297eb82ba045d206147473468b6061
DesJC <jcd9930@hotmail.ca>
2018-03-29 21:33:16 -0400

    Add GridTest Class

---
+++ TrafficLibrary/TrafficTest/GridTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TrafficTest
+{
+    [TestClass]
+    public class GridTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
