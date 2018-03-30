8894ba7c5b8d8eafbb59b82a3a558c51c8a432dd
DesJC <jcd9930@hotmail.ca>
2018-03-29 21:27:14 -0400

    Add IntersectionTileTest Class

---
+++ TrafficLibrary/TrafficTest/IntersectionTileTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TrafficTest
+{
+    [TestClass]
+    public class IntersectionTileTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
