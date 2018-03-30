b3b0ba3f7b8d0aead61ef34739a5897b43faa2f0
DesJC <jcd9930@hotmail.ca>
2018-03-29 20:20:29 -0400

    Add LightTest class

---
+++ TrafficLibrary/TrafficTest/LightTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using TrafficLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TrafficTest
+{
+    [TestClass]
+    public class LightTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
