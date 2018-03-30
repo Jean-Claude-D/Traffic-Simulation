83046602e3caa0babdd201480cf66d9105e718ce
DesJC <jcd9930@hotmail.ca>
2018-03-29 21:12:27 -0400

    Add test cases in LightTest

--- TrafficLibrary/TrafficTest/LightTest.cs
+++ TrafficLibrary/TrafficTest/LightTest.cs
@@ -7,9 +7,59 @@ namespace TrafficTest
     [TestClass]
     public class LightTest
     {
+        private static int[] _timings = { 20, 5, 10, 5 };
+
         [TestMethod]
-        public void TestMethod1()
+        public void LightConstructor_ValidISignalstrategy()
         {
+
+        }
+
+        [TestMethod]
+        public void LightConstructor_NullISignalstrategy()
+        {
+
+        }
+
+        [TestMethod]
+        public void LightConstructor_ValidDirection()
+        {
+
+        }
+
+        [TestMethod]
+        public void LightConstructor_DirectionNone()
+        {
+
+        }
+
+        [TestMethod]
+        public void Light_GetColourFirstCycle()
+        {
+
+        }
+
+        [TestMethod]
+        public void Light_GetColourSecondCycle()
+        {
+
+        }
+
+        [TestMethod]
+        public void Light_GetColourThirdCycle()
+        {
+
+        }
+
+        [TestMethod]
+        public void Light_GetColourFourthCycle()
+        {
+
+        }
+
+        public ISignalStrategy getTrafficLight()
+        {
+            return new FixedSignal(_timings);
         }
     }
 }
