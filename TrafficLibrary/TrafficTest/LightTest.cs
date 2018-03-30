758867b4862cd94bfe1283d58d4389dd3f75dfdd
DesJC <jcd9930@hotmail.ca>
2018-03-29 21:23:05 -0400

    Code test cases in LightTest Class

--- TrafficLibrary/TrafficTest/LightTest.cs
+++ TrafficLibrary/TrafficTest/LightTest.cs
@@ -10,54 +10,83 @@ namespace TrafficTest
         private static int[] _timings = { 20, 5, 10, 5 };
 
         [TestMethod]
-        public void LightConstructor_ValidISignalstrategy()
+        public void LightConstructor_Valid()
         {
+            Light myLight;
+            ISignalStrategy myTrafficLight = getTrafficLight();
+            Direction myDirection = Direction.Right;
 
+            myLight = new Light(myTrafficLight, myDirection);
         }
 
         [TestMethod]
         public void LightConstructor_NullISignalstrategy()
         {
+            Light myLight;
+            ISignalStrategy myTrafficLight = null;
+            Direction myDirection = Direction.Right;
 
-        }
-
-        [TestMethod]
-        public void LightConstructor_ValidDirection()
-        {
-
+            Assert.ThrowsException<ArgumentException>(
+                () => myLight = new Light(myTrafficLight, myDirection));
         }
 
         [TestMethod]
         public void LightConstructor_DirectionNone()
         {
+            Light myLight;
+            ISignalStrategy myTrafficLight = getTrafficLight();
+            Direction myDirection = Direction.None;
 
+            Assert.ThrowsException<ArgumentException>(
+                () => myLight = new Light(myTrafficLight, myDirection));
         }
 
         [TestMethod]
         public void Light_GetColourFirstCycle()
         {
+            ISignalStrategy myTrafficLight = getTrafficLight();
+            Direction myDirection = Direction.Right;
+            Light myLight = new Light(myTrafficLight, myDirection);
 
+            for(int i = 0; i < _timings[0] - 1; i++)
+            {
+                myTrafficLight.Update();
+            }
+
+            Assert.AreEqual(Colour.Green, myLight.colour);
         }
 
         [TestMethod]
         public void Light_GetColourSecondCycle()
         {
+            ISignalStrategy myTrafficLight = getTrafficLight();
+            Direction myDirection = Direction.Right;
+            Light myLight = new Light(myTrafficLight, myDirection);
 
+            for (int i = 0; i < (_timings[1] - 1) + _timings[0] ; i++)
+            {
+                myTrafficLight.Update();
+            }
+
+            Assert.AreEqual(Colour.Amber, myLight.colour);
         }
 
         [TestMethod]
         public void Light_GetColourThirdCycle()
         {
+            ISignalStrategy myTrafficLight = getTrafficLight();
+            Direction myDirection = Direction.Right;
+            Light myLight = new Light(myTrafficLight, myDirection);
 
+            for (int i = 0; i < (_timings[2] - 1) + _timings[1] + _timings[0]; i++)
+            {
+                myTrafficLight.Update();
+            }
+
+            Assert.AreEqual(Colour.Red, myLight.colour);
         }
 
-        [TestMethod]
-        public void Light_GetColourFourthCycle()
-        {
-
-        }
-
-        public ISignalStrategy getTrafficLight()
+        private ISignalStrategy getTrafficLight()
         {
             return new FixedSignal(_timings);
         }
