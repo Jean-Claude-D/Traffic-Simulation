5675091559453e3e2aac34b5cfa3d7225f9724b1
Quoc Huan Nguyen <quoc.nguyen@dawsoncollege.qc.ca>
2018-04-09 13:54:39 -0400

    Setup methods for TotalSprite.cs

--- TrafficLibrary/TrafficSimulation/TotalSprite.cs
+++ TrafficLibrary/TrafficSimulation/TotalSprite.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +12,39 @@ using System.Threading.Tasks;
 
 namespace TrafficSimulation
 {
-    class TotalSprite
+    class TotalSprite : DrawableGameComponent
     {
+
+        //to render
+        private SpriteBatch spriteBatch;
+        private Texture2D totalImage;
+        private Simulation sim;
+
+        public TotalSprite(Simulation sim) : base(sim)
+        {
+            this.sim = sim;
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            base.LoadContent();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            base.Draw(gameTime);
+        }
+
 
     }
 }
