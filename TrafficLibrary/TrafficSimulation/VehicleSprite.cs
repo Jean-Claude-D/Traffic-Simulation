682366f7a23c5c12b737192580fb7327e520fd03
benjii <benjaminjameskearney@gmail.com>
2018-04-09 13:44:31 -0400

    start VehicleSprite

---
+++ TrafficLibrary/TrafficSimulation/VehicleSprite.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using TrafficLibrary;
+
+namespace TrafficSimulation
+{
+    public class VehicleSprite : DrawableGameComponent
+    {
+        public VehicleSprite(Game1 game) : base(game)
+        {
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            base.Draw(gameTime);
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        protected override void LoadContent()
+        {
+            base.LoadContent();
+        }
+    }
+}
