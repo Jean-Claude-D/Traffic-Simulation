267c3d714758a4c141bfd55950643f77d971c9ee
DesJC <jcd9930@hotmail.ca>
2018-04-10 13:08:30 -0400

    Add documentation for Motorcycle Class

--- TrafficLibrary/TrafficLibrary/Motorcycle.cs
+++ TrafficLibrary/TrafficLibrary/Motorcycle.cs
@@ -6,13 +6,28 @@ using System.Threading.Tasks;
 
 namespace TrafficLibrary
 {
+    /// <summary>
+    /// Standard Vehicle implementation with
+    /// light emission level and one passengers
+    /// </summary>
     public class Motorcycle:Vehicle
     {
+        /// <summary>
+        /// Creates a Motorcycle object
+        /// on a specific Grid
+        /// </summary>
+        /// <param name="grid">The Grid on which the Motocycle is</param>
         public Motorcycle(Grid grid): base(2, 1, 1, grid)
-        {
-        }
+        { }
+
+        /// <summary>
+        /// Creates a Motocycle object on
+        /// Grid at a specific position
+        /// </summary>
+        /// <param name="grid">The Grid on which the Motorcycle is</param>
+        /// <param name="x">The column of the tile on which the Motorcycle is</param>
+        /// <param name="y">The row of the tile on which the Motorcycle is</param>
         public Motorcycle(Grid grid, int x, int y) : base(2, 1, 1, grid, x, y)
-        {
-        }
+        { }
     }
 }
