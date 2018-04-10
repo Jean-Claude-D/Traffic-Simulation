23a318e0dcc54cc0a9d505ff1ecdb65c24fbd29d
DesJC <jcd9930@hotmail.ca>
2018-04-10 13:06:14 -0400

    Add documentation for Car Class

--- TrafficLibrary/TrafficLibrary/Car.cs
+++ TrafficLibrary/TrafficLibrary/Car.cs
@@ -6,13 +6,28 @@ using System.Threading.Tasks;
 
 namespace TrafficLibrary
 {
+    /// <summary>
+    /// Standard Vehicle implementation with
+    /// medium emission and passengers level
+    /// </summary>
     public class Car:Vehicle
     {
+        /// <summary>
+        /// Creates a Car object
+        /// on a specific Grid
+        /// </summary>
+        /// <param name="grid">The Grid on which the Car is</param>
         public Car(Grid grid): base(5, 2, 3, grid)
-        {
-        }
+        { }
+
+        /// <summary>
+        /// Creates a Car object on
+        /// Grid at a specific position
+        /// </summary>
+        /// <param name="grid">The Grid on which the Car is</param>
+        /// <param name="x">The column of the tile on which the Car is</param>
+        /// <param name="y">The row of the tile on which the Car is</param>
         public Car(Grid grid, int x, int y) : base(5, 2, 3, grid, x, y)
-        {
-        }
+        { }
     }
 }
