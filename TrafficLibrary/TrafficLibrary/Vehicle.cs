2b2e526cc0ee0e41e906ac02006e9025d306bf48
DesJC <jcd9930@hotmail.ca>
2018-04-09 22:25:21 -0400

    Modify Vehicle Class code

--- TrafficLibrary/TrafficLibrary/Vehicle.cs
+++ TrafficLibrary/TrafficLibrary/Vehicle.cs
@@ -6,64 +6,82 @@ using System.Threading.Tasks;
 
 namespace TrafficLibrary
 {
+    /// <summary>
+    /// Basic implementation of IVehicle
+    /// </summary>
     public abstract class Vehicle : IVehicle
     {
-        private Direction direction;
-        private int x;
-        private int y;
-        private int passengers;
-        private double emissionIdle;
-        private double emissionMoving;
-        private Grid grid;
-
         public Vehicle(double emissionMoving, double emissionIdle, int passengers, Grid grid)
             : this(emissionMoving, emissionIdle, passengers, grid, default(int), default(int))
         { }
 
         public Vehicle(double emissionMoving, double emissionIdle, int passengers, Grid grid, int x, int y)
         {
-             if (grid != null)
+            if(grid == null)
             {
-                this.emissionMoving = emissionMoving;
-                this.emissionIdle = emissionIdle;
-                this.passengers = passengers;
-                this.grid = grid;
-                this.x = x;
-                this.y = y;
-                this.direction = grid[x, y].Direction;
+                throw new ArgumentException("grid cannot be null");
             }
-            else
-            {
-                throw new ArgumentException("Null Grid!");
-            }
+
+            this._grid = grid;
+            this.Direction = grid[x, y].Direction;
+
+            this.X = x;
+            this.Y = y;
+
+            this.Passengers = passengers;
+
+            this.EmissionMoving = emissionMoving;
+            this.EmissionIdle = emissionIdle;
+        }
+
+        private Grid _grid;
+
+        private Boolean isLocationAnIntersection(int x, int y)
+        {
+            return !isLocationOutOfBounds(x, y) &&
+                _grid[x, y].GetType().Equals(typeof(IntersectionTile));
         }
 
         public Direction Direction
         {
-            get { return this.direction; }
-            set { this.direction = value; }
+            get;
+            set;
         }
+
         public int X
         {
-            get { return this.x;  }
-            set { this.x = value; }
+            get;
+            set;
         }
         public int Y
         {
-            get { return this.y; }
-            set { this.y = value; }
+            get;
+            set;
         }
+
+        private Boolean isOutOfBounds()
+        {
+            return isLocationOutOfBounds(X, Y);
+        }
+
+        private Boolean isLocationOutOfBounds(int x, int y)
+        {
+            return x >= _grid.Size || y >= _grid.Size
+                || x < 0 || y < 0;
+        }
+
         public int Passengers
         {
-            get { return this.passengers; }
+            get;
         }
+
         public double EmissionIdle
         {
-            get { return this.emissionIdle; }
+            get;
         }
         public double EmissionMoving
         {
-            get { return this.emissionMoving; }
+            get;
         }
 
         public event IVehicleHandler Done;
@@ -72,94 +90,48 @@ namespace TrafficLibrary
 
         public bool InIntersection()
         {
-            if(grid[x,y].GetType().Equals(typeof(IntersectionTile)))
-            {
-                grid[x,y].Occupied = true;
-                return true;
-            }
-            else
-            {
-                return false;
-            }
+            return isOutOfBounds() || isLocationAnIntersection(X, Y);
         }
 
         public void Move(ISignalStrategy signal)
         {
-            if ((!NextIsIntersection()) || InIntersection() || signal.GetColour(this.direction) == Colour.Green)
+            switch (this.Direction)
             {
-                switch (this.direction)
-                {
-                    case Direction.Down:
-                        if (y + 1 >= grid.Size)
-                        {
-                            Done?.Invoke(this);
-                            grid[x, y].Occupied = false;
-                            break;
-                        }
-                        else if (!grid[x, y + 1].Occupied)
-                        {
-                            this.y++;
-                            grid[x, y].Occupied = true;
-                            grid[x, y - 1].Occupied = false;
-                        }
-                        else
-                        {
-                            Waiting?.Invoke(this);
-                        }
-                        break;
-                    case Direction.Up:
-                        if (y - 1 < 0)
-                        {
-                            Done?.Invoke(this);
-                            grid[x, y].Occupied = false;
-                            break;
-                        }
-                        else if (!grid[x, y - 1].Occupied)
-                        {
-                            this.y--;
-                            grid[x, y].Occupied = true;
-                            grid[x, y + 1].Occupied = false;
-                        }
-                        else
-                        {
-                            Waiting?.Invoke(this);
-                        }
-                        break;
-                    case Direction.Left:
-                        if (x - 1 < 0)
-                        {
-                            Done?.Invoke(this);
-                            grid[x, y].Occupied = false;
-                            break;
-                        }
-                        else if (!grid[x - 1, y].Occupied)
-                        {
-                            this.x--;
-                            grid[x, y].Occupied = true;
-                            grid[x + 1, y].Occupied = false;
-                        }
-                        else
-                        {
-                            Waiting?.Invoke(this);
-                        }
-                        break;
-                    case Direction.Right:
-                        if (x + 1 >= grid.Size)
-                        {
-                            Done?.Invoke(this);
-                            grid[x, y].Occupied = false;
-                        }
-                        else if (!grid[x + 1, y].Occupied)
-                        {
-                            this.x++;
-                            grid[x, y].Occupied = true;
-                            grid[x - 1, y].Occupied = false;
-                        }
-                        break;
-                    default:
-                        Waiting?.Invoke(this);
-                        break;
-                }
+                case Direction.Down:
+                    move(X, Y + 1, signal);
+                    break;
+                case Direction.Up:
+                    move(X, Y - 1, signal);
+                    break;
+                case Direction.Left:
+                    move(X - 1, Y, signal);
+                    break;
+                case Direction.Right:
+                    move(X + 1, Y, signal);
+                    break;
+                default:
+                    throw new Exception
+                        ("Direction of this Vehicle (" + Direction + ") should never be Direction.None");
+            }
+        }
+
+        private void move(int x, int y, ISignalStrategy signal)
+        {
+
+            if(isLocationOutOfBounds(x, y))
+            {
+                _grid[X, Y].Occupied = false;
+                Done?.Invoke(this);
+            }
+            else if(((!NextIsIntersection()) ||
+                InIntersection() ||
+                signal.GetColour(this.Direction) == Colour.Green)
+                && !_grid[x, y].Occupied)
+            {
+                _grid[X, Y].Occupied = false;
+                X = x;
+                Y = y;
+                _grid[X, Y].Occupied = true;
                 Moved?.Invoke(this);
             }
             else
@@ -171,60 +143,16 @@ namespace TrafficLibrary
 
         public bool NextIsIntersection()
         {
-            switch (this.direction)
+            switch (this.Direction)
             {
                 case Direction.Down:
-                    if(y + 1 >= grid.Size)
-                    {
-                        return false;
-                    }
-                    else if (grid[x, y+1].GetType().Equals(typeof(IntersectionTile)))
-                    {
-                        return true;
-                    }
-                    else
-                    {
-                        return false;
-                    }
+                    return isLocationAnIntersection(X, Y + 1);
                 case Direction.Up:
-                    if (y - 1 < 0)
-                    {
-                        return false;
-                    }
-                    else if (grid[x, y-1].GetType().Equals(typeof(IntersectionTile)))
-                    {
-                        return true;
-                    }
-                    else
-                    {
-                        return false;
-                    }
+                    return isLocationAnIntersection(X, Y - 1);
                 case Direction.Left:
-                    if (x - 1 < 0)
-                    {
-                        return false;
-                    }
-                    else if (grid[x-1, y].GetType().Equals(typeof(IntersectionTile)))
-                    {
-                        return true;
-                    }
-                    else
-                    {
-                        return false;
-                    }
+                    return isLocationAnIntersection(X - 1, Y);
                 case Direction.Right:
-                    if (x + 1 >= grid.Size)
-                    {
-                        return false;
-                    }
-                    else if (grid[x+1, y].GetType().Equals(typeof(IntersectionTile)))
-                    {
-                        return true;
-                    }
-                    else
-                    {
-                        return false;
-                    }
+                    return isLocationAnIntersection(X + 1, Y);
                 default:
                     return false;
             };
