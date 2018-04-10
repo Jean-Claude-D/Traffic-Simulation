214f98e69c72b0581dd924feac7b533b8f52c464
DesJC <jcd9930@hotmail.ca>
2018-04-10 13:39:29 -0400

    Add documentation for ISignalStrategy

--- TrafficLibrary/TrafficLibrary/ISignalStrategy.cs
+++ TrafficLibrary/TrafficLibrary/ISignalStrategy.cs
@@ -6,9 +6,26 @@ using System.Threading.Tasks;
 
 namespace TrafficLibrary
 {
+    /// <summary>
+    /// Strategy used for changing
+    /// traffic lights colours
+    /// </summary>
     public interface ISignalStrategy
     {
+        /// <summary>
+        /// Updates this ISignalStrategy
+        /// (every simulation tick)
+        /// </summary>
         void Update();
+
+        /// <summary>
+        /// Gets the current Colour of this
+        /// ISignalStrategy for a given Direction
+        /// </summary>
+        /// <param name="dir">The Direction to
+        /// get the colour for</param>
+        /// <returns>The Colour this
+        /// traffic light shows</returns>
         Colour GetColour(Direction dir);
     }
 }
